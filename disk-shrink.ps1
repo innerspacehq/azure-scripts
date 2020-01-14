96e99d31bdb76f243df4f329b2eac4c5732d920b
Matt MacGillivray <qmnonic@gmail.com>
2020-01-14 12:24:09 -0500

    Changes suggested by David Ifemeni on 2020/01/14

--- disk-shrink.ps1
+++ disk-shrink.ps1
@@ -1,9 +1,10 @@
 # Variables
-$DiskID = ""# eg. "/subscriptions/203bdbf0-69bd-1a12-a894-a826cf0a34c8/resourcegroups/rg-server1-prod-1/providers/Microsoft.Compute/disks/Server1-Server1"
+
+$DiskID = ""# eg. "/subscriptions/203bdbf0-69bd-1a12-a894-a826cf0a3/resourcegroups/rg-server1-prod-1/providers/Microsoft.Compute/disks/Server1-Server1"
 $VMName = "VM-Server1"
 $DiskSizeGB = 30
 $AzSubscription = "Prod Subscription"
-
+ 
 # Script
 # Provide your Azure admin credentials
 Connect-AzAccount
@@ -24,30 +25,19 @@ $DiskName = $Disk.Name
 # Get SAS URI for the Managed disk
 $SAS = Grant-AzDiskAccess -ResourceGroupName $resourceGroupName -DiskName $DiskName -Access 'Read' -DurationInSecond 600000;
 
-#Provide the managed disk name
-#$managedDiskName = "yourManagedDiskName" 
-
-#Provide Shared Access Signature (SAS) expiry duration in seconds e.g. 3600.
-#$sasExpiryDuration = "3600"
-
 #Provide storage account name where you want to copy the snapshot - the script will create a new one temporarily
 $storageAccountName = "sashrinkddisk" + ($($VMName -replace '[^a-zA-Z0-9]', '')).ToLower()
 
 #Name of the storage container where the downloaded snapshot will be stored
 $storageContainerName = $storageAccountName
 
-#Provide the key of the storage account where you want to copy snapshot. 
-#$storageAccountKey = "yourStorageAccountKey"
-
 #Provide the name of the VHD file to which snapshot will be copied.
 $destinationVHDFileName = "$($VM.StorageProfile.OsDisk.Name).vhd"
 
-#Generate the SAS for the managed disk
-#$sas = Grant-AzureRmDiskAccess -ResourceGroupName $resourceGroupName -DiskName $managedDiskName -Access Read -DurationInSecond $sasExpiryDuration
-
-#Create the context for the storage account which will be used to copy snapshot to the storage account 
+#Create the context for the storage account which will be used to copy snapshot to the storage account
 $StorageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -SkuName Standard_LRS -Location $VM.Location
 $destinationContext = $StorageAccount.Context
+
 $container = New-AzStorageContainer -Name $storageContainerName -Permission Off -Context $destinationContext
 
 #Copy the snapshot to the storage account and wait for it to complete
@@ -55,14 +45,12 @@ Start-AzStorageBlobCopy -AbsoluteUri $SAS.AccessSAS -DestContainer $storageConta
 while(($state = Get-AzStorageBlobCopyState -Context $destinationContext -Blob $destinationVHDFileName -Container $storageContainerName).Status -ne "Success") { $state; Start-Sleep -Seconds 20 }
 $state
 
+ 
 # Revoke SAS token
 Revoke-AzDiskAccess -ResourceGroupName $resourceGroupName -DiskName $DiskName
 
-# Empty disk to get footer from
+# Emtpy disk to get footer from
 $emptydiskforfootername = "$($VM.StorageProfile.OsDisk.Name)-empty.vhd"
-
-# Empty disk URI
-#$EmptyDiskURI = $container.CloudBlobContainer.Uri.AbsoluteUri + "/" + $emptydiskforfooter
 
 $diskConfig = New-AzDiskConfig `
     -Location $VM.Location `
@@ -73,7 +61,7 @@ $dataDisk = New-AzDisk `
     -ResourceGroupName $resourceGroupName `
     -DiskName $emptydiskforfootername `
     -Disk $diskConfig
-
+ 
 $VM = Add-AzVMDataDisk `
     -VM $VM `
     -Name $emptydiskforfootername `
@@ -85,7 +73,7 @@ Update-AzVM -ResourceGroupName $resourceGroupName -VM $VM
 
 $VM | Stop-AzVM -Force
 
-
+ 
 # Get SAS token for the empty disk
 $SAS = Grant-AzDiskAccess -ResourceGroupName $resourceGroupName -DiskName $emptydiskforfootername -Access 'Read' -DurationInSecond 600000;
 
@@ -93,6 +81,7 @@ $SAS = Grant-AzDiskAccess -ResourceGroupName $resourceGroupName -DiskName $empty
 Start-AzStorageBlobCopy -AbsoluteUri $SAS.AccessSAS -DestContainer $storageContainerName -DestBlob $emptydiskforfootername -DestContext $destinationContext
 while(($state = Get-AzStorageBlobCopyState -Context $destinationContext -Blob $emptydiskforfootername -Container $storageContainerName).Status -ne "Success") { $state; Start-Sleep -Seconds 20 }
 $state
+
 
 # Revoke SAS token
 Revoke-AzDiskAccess -ResourceGroupName $resourceGroupName -DiskName $emptydiskforfootername
@@ -104,18 +93,24 @@ Update-AzVM -ResourceGroupName $resourceGroupName -VM $VM
 # Delete temp disk
 Remove-AzDisk -ResourceGroupName $resourceGroupName -DiskName $emptydiskforfootername -Force;
 
+ 
 # Get the blobs
 $emptyDiskblob = Get-AzStorageBlob -Context $destinationContext -Container $storageContainerName -Blob $emptydiskforfootername
 $osdisk = Get-AzStorageBlob -Context $destinationContext -Container $storageContainerName -Blob $destinationVHDFileName
+ 
+
 $footer = New-Object -TypeName byte[] -ArgumentList 512
 write-output "Get footer of empty disk"
+
 $downloaded = $emptyDiskblob.ICloudBlob.DownloadRangeToByteArray($footer, 0, $emptyDiskblob.Length - 512, 512)
+
 $osDisk.ICloudBlob.Resize($emptyDiskblob.Length)
 $footerStream = New-Object -TypeName System.IO.MemoryStream -ArgumentList (,$footer)
 write-output "Write footer of empty disk to OSDisk"
 $osDisk.ICloudBlob.WritePages($footerStream, $emptyDiskblob.Length - 512)
 Write-Output -InputObject "Removing empty disk blobs"
 $emptyDiskblob | Remove-AzStorageBlob -Force
+
 
 #Provide the name of the Managed Disk
 $NewDiskName = "$DiskName" + "-new"
